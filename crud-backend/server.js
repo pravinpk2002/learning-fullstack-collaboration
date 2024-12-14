8c8ac01a775be78826a59a6622fdef96ce45ef52
Bhagvat Mutthe <bhagvatmutthe6@gmail.com>
2024-12-14 22:25:21 +0530

    secure database credentials using .env file

--- crud-backend/server.js
+++ crud-backend/server.js
@@ -8,11 +8,11 @@ const port = 3000;
 
 // MySQL Database connection
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root", 
-  password: "",
-  database: "crud_example",
-  port: "3308", 
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
